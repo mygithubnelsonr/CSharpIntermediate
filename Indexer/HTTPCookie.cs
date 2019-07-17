c7ad785c04a7760bccecc75f3a4a24e24cf57189
Robert Nelson <reg0204@sys-service.de>
2019-07-17 17:00:28 +0200

    add the indexer project

---
+++ Indexer/HTTPCookie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Indexer
+{
+    public class HTTPCookie
+    {
+        private readonly Dictionary<string, string> _dictionary;
+        public DateTime Expire { get; set; }
+
+        public HTTPCookie()
+        {
+            _dictionary = new Dictionary<string, string>();
+        }
+
+        // create an indexer
+        public string this[string key]
+        {
+            get { return _dictionary[key]; }
+            set { _dictionary[key] = value; }
+        }
+    }
+}
---
+++ Indexer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cookie = new HTTPCookie();
+
+            cookie["name"] = "Mosh";
+            cookie.Expire = new DateTime(2019, 07, 18);
+
+            Console.WriteLine($"cookie name = {cookie["name"]}, expire date = {cookie.Expire}");
+
+            Console.ReadKey();
+        }
+    }
+}
